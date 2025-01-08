4d3b636f7ad626cedf728357f93cf726e490b1d4
Ana Vera <ana.clara.vera13@gmail.com>
2025-01-08 15:00:04 -0300

    feat: specPattern :sparkles:

--- cypress.config.js
+++ cypress.config.js
@@ -10,12 +10,17 @@ module.exports = defineConfig({
       config.env = { ...config.env, ...env };
       config.viewportWidth = 1920;
       config.viewportHeight = 1080;
-      
+
       return config;
     },
     baseUrl: 'https://www.saucedemo.com',
     chromeWebSecurity: false,
     defaultCommandTimeout: 10000,
+    specPattern: [
+      'cypress/e2e/autenticacao/login.cy.js',
+      'cypress/e2e/compra/carrinho.cy.js',
+      'cypress/e2e/checkout/checkout.cy.js',
+    ], //utilizei do specPattern para organizar a ordem da execução dos testes.
     env: { ...process.env },
   },
 });
